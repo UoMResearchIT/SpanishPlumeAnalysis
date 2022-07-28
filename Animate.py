a467ba6674242d270e90117e94e1439e83554309
fherreazcue <francisco.herreazcue@gmail.com>
2022-07-28 10:48:21 +0100

    Loads all wrfout files in dir_path -Concatenated all wrfout files within a directory -Updated removal of png files to a safer mode.

--- Animate.py
+++ Animate.py
@@ -3,35 +3,48 @@ from datetime import datetime      	############################################
 from netCDF4 import Dataset
 from Plot2DField import *
 import imageio
-import subprocess
+import os
 
 print(datetime.now())           	###############################################
 
-Files=[]
 
-# Open the NetCDF file
-dir_n="/mnt/seaes01-data01/dmg/dmg/mbessdl2/Spanish_Plume/WRF/run-zrek/"
-wrf_fn="wrfout_d01_2015-06-29_00:00:00"
-print("Loading ",wrf_fn)
-ncfile = Dataset(dir_n+wrf_fn)
+WRFfiles=[]
+PNGfiles=[]
 
-# Get number of time slices and plot them
-timerange=ncfile.variables['Times'].shape[0]
-#timerange=4
-for ti in range(timerange):
-	of=wrf_fn+"_t_"+str(ti)+".png"
-	Files.append(of)
-	print("Processing:",ti+1,"/",timerange, end = '\r')
-	Plot2DField(ncfile,"slp","Sea level pressure [hPa] - t="+str(ti),ti,of)
-print("Processed successfully.")
+
+dir_path="/mnt/seaes01-data01/dmg/dmg/mbessdl2/Spanish_Plume/WRF/run-zrek/"
+
+#Get list of files from directoy
+for file in os.listdir(dir_path):
+    # check only wrfout files
+    if file.startswith('wrfout'):
+        WRFfiles.append(file)
+WRFfiles.sort()
+
+#Plots each time slice in each file
+for wrf_fn in WRFfiles:
+	# Open the NetCDF file
+	print("Loading ",wrf_fn)
+	ncfile = Dataset(dir_path+wrf_fn)
+
+	# Get number of time slices and plot them
+	timerange=ncfile.variables['Times'].shape[0]
+#	if timerange>3:timerange=3
+	for ti in range(timerange):
+		of=wrf_fn+"_t_"+str(ti)+".png"
+		PNGfiles.append(of)
+		print("Processing:",ti+1,"/",timerange, end = '\r')
+		Plot2DField(ncfile,"slp","Sea level pressure [hPa] - t="+str(ti),ti,of)
+	print("Processed successfully.")
 
 # Build GIF
 with imageio.get_writer('SLP.gif', mode='I') as writer:
-    for filename in Files:
+    for filename in PNGfiles:
         image = imageio.imread(filename)
         writer.append_data(image)
 		
 # Remove individual frames
-subprocess.run(['rm ./*.png'],shell=True)
+for file in PNGfiles:
+	os.remove(file)
 
 print(datetime.now())           	###############################################
