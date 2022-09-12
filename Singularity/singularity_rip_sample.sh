96695b8535d2b19355486bb22ed32655066c2178
fherreazcue <francisco.herreazcue@gmail.com>
2022-09-12 10:16:02 +0100

    Sample data and run script of singularity rip.

---
+++ Singularity/singularity_rip_sample.sh
@@ -0,0 +1,8 @@
+singularity \
+    exec \
+        --contain \
+        --cleanenv \
+        --pwd /Sample2 \
+        --bind /mnt/seaes01-data01/dmg/dmg/mbcxpfh2/SpanishPlume/Analysis/Singularity/Sample/:/Sample2/ \
+        ripdocker_latest.sif  \
+        /bin/bash run.sh
