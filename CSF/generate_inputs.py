5a3d1e80cd9717dd37d7ca47d07a86b8382fcf98
fherreazcue <francisco.herreazcue@gmail.com>
2024-05-14 17:37:29 +0100

    Adds input generation for csv data

--- CSF/generate_inputs.py
+++ CSF/generate_inputs.py
@@ -10,15 +10,15 @@ base_dir_path = "/mnt/seaes01-data01/dmg/dmg/mbessdl2/Spanish_Plume/WRF/"
 simulations = [
     # {"wrfout":"control",                            "output":"rip"},      # For local tests called from src directory
     {"output": "Control", "wrfout": "run-zrek"},
-    {"output": "G_Wave_fix", "wrfout": "run-zrek.gravity_wave.CON_OA_fix"},
+    # {"output": "G_Wave_fix", "wrfout": "run-zrek.gravity_wave.CON_OA_fix"},
     {"output": "Double_hgt", "wrfout": "run-zrek.double_hgt"},
     {"output": "Half_hgt", "wrfout": "run-zrek.half_hgt"},
     {"output": "Zero_hgt", "wrfout": "run-zrek.zero_hgt"},
     {"output": "Albedo_90", "wrfout": "run-zrek.albedo_90"},
     {"output": "Albedo_Standard", "wrfout": "run-zrek.albedo_standard"},
     {"output": "Albedo_40", "wrfout": "run-zrek.albedo_40"},
-    {"output": "Morrison", "wrfout": "run-zrek.morrison_microphysics"},
-    {"output": "Thompson", "wrfout": "run-zrek.thompson_microphysics"},
+    # {"output": "Morrison", "wrfout": "run-zrek.morrison_microphysics"},
+    # {"output": "Thompson", "wrfout": "run-zrek.thompson_microphysics"},
     {"output": "Dom2", "wrfout": "run-zrek.2domains"},
 ]
 
@@ -81,6 +81,31 @@ with open("all_skewt.inputs", "w") as file:
         for skewt in skewts:
             task = "--task=diagnostic"
             var = f"--var={skewt}"
+            dir_path = f"--dir_path={base_dir_path}{simulation['wrfout']}/"
+            opts_dom = opts
+            if simulation["output"] in ["Albedo_90", "Dom2"]:
+                opts_dom = opts + " --domain=full"
+            outdir = f"--outdir={simulation['output']}/"
+
+            file.write(
+                f"{task:<{pad_task}} {var:<{pad_var}} {dir_path:<{pad_wrf}} {opts_dom:<{pad_opts}} {outdir}\n"
+            )
+        file.write("\n")
+
+# Generate csv inputs
+locations = [
+    "Nottingham",
+    "Larkhill",
+    "BristolChannel",
+    "Caerphilly",
+    "Bath",
+]
+pad_var = max(len(location) for location in locations) + 11
+with open("all_csv.inputs", "w") as file:
+    for simulation in simulations:
+        for location in locations:
+            task = "--task=csv"
+            var = f"--var=CSV_{location}"
             dir_path = f"--dir_path={base_dir_path}{simulation['wrfout']}/"
             opts_dom = opts
             if simulation["output"] in ["Albedo_90", "Dom2"]:
