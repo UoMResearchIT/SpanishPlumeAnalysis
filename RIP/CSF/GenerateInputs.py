98945b2291f4973adfdcc7da704ea09241c0e58e
fherreazcue <francisco.herreazcue@gmail.com>
2024-05-22 16:35:36 +0100

    Includes diagnostics in trajectory input generation

--- RIP/CSF/GenerateInputs.py
+++ RIP/CSF/GenerateInputs.py
@@ -4,46 +4,52 @@
 simulations = [
     # {"wrfout":"control",                            "output":"rip"},      # For local tests called from src directory
     {"wrfout": "run-zrek", "output": "Control"},
-    {"wrfout": "run-zrek.gravity_wave.CON_OA_fix", "output": "G_Wave_fix"},
-    {"wrfout": "run-zrek.double_hgt", "output": "Double_hgt"},
-    {"wrfout": "run-zrek.half_hgt", "output": "Half_hgt"},
-    {"wrfout": "run-zrek.zero_hgt", "output": "Zero_hgt"},
+    # {"wrfout": "run-zrek.gravity_wave.CON_OA_fix", "output": "G_Wave_fix"},
+    # {"wrfout": "run-zrek.double_hgt", "output": "Double_hgt"},
+    # {"wrfout": "run-zrek.half_hgt", "output": "Half_hgt"},
+    # {"wrfout": "run-zrek.zero_hgt", "output": "Zero_hgt"},
     {"wrfout": "run-zrek.albedo_90", "output": "Albedo_90"},
     {"wrfout": "run-zrek.albedo_standard", "output": "Albedo_Standard"},
     {"wrfout": "run-zrek.albedo_40", "output": "Albedo_40"},
-    {"wrfout": "run-zrek.morrison_microphysics", "output": "Morrison"},
-    {"wrfout": "run-zrek.thompson_microphysics", "output": "Thompson"},
+    # {"wrfout": "run-zrek.morrison_microphysics", "output": "Morrison"},
+    # {"wrfout": "run-zrek.thompson_microphysics", "output": "Thompson"},
 ]
 # List of trajectories
 # Aberporth,Algeria,Camborne,Gibraltar,Herstmonceux,LaCoruna,Larkhill,Lerwick,Madrid,Murcia,Nimes,Nottingham,Santander,Stornoway,Trappes
 trajectory_locations = [
     # {"name":"Aberporth",    "x":"179", "y":"407"},
-    {"name": "Algeria", "x": "172", "y": "251"},
+    # {"name": "Algeria", "x": "172", "y": "251"},
     # {"name":"Camborne",     "x":"173", "y":"392"},
-    {"name": "Gibraltar", "x": "158", "y": "287"},
+    # {"name": "Gibraltar", "x": "158", "y": "287"},
     # {"name":"Herstmonceux"  "x":"202", "y":"395"},
-    {"name": "LaCoruna", "x": "149", "y": "343"},
+    # {"name": "LaCoruna", "x": "149", "y": "343"},
     # {"name":"Larkhill",     "x":"191", "y":"398"},
     # {"name":"Lerwick",      "x":"202", "y":"470"},
-    {"name": "Madrid", "x": "172", "y": "317"},
-    {"name": "Murcia", "x": "184", "y": "297"},
-    {"name": "Nimes", "x": "219", "y": "339"},
+    # {"name": "Madrid", "x": "172", "y": "317"},
+    # {"name": "Murcia", "x": "184", "y": "297"},
+    # {"name": "Nimes", "x": "219", "y": "339"},
     {"name": "Nottingham", "x": "195", "y": "412"},
-    {"name": "Santander", "x": "174", "y": "340"},
+    # {"name": "Santander", "x": "174", "y": "340"},
     # {"name":"Stornoway",    "x":"178", "y":"457"},
-    {"name": "Trappes", "x": "209", "y": "377"},
+    # {"name": "Trappes", "x": "209", "y": "377"},
 ]
 trajectory_times = [
     # Forward
     # "38-68",
     # "92-122",
-    "0-80",
-    "45-125",
+    # "0-80",
+    # "45-125",
     # Backward
+    "60-0",
+    "66-0",
+    "114-54",
+    "114-0",
+    "120-54",
+    "120-0",
     # "68-38",
     # "122-92",
-    "80-0",
-    "125-45",
+    # "80-0",
+    # "125-45",
 ]
 # Build trajectory list
 trajectory_list = []
@@ -57,6 +63,14 @@ for location in trajectory_locations:
                 "name": location["name"],
             }
         )
+# Diagnostics
+diags = [
+    # "none",
+    # "",
+    "g1",
+    "g2",
+    # "all",
+]
 
 wrfout_dir = "/mnt/seaes01-data01/dmg/dmg/mbessdl2/Spanish_Plume/WRF/"
 rip_dir = "/mnt/seaes01-data01/dmg/dmg/mbcxpfh2/SpanishPlume/Analysis/RIP/"
@@ -84,16 +98,18 @@ with open(name, "w") as file:
     # Iterate through trajectories and simulations (trajectories first to prevent errors when creating folders)
     for trajectory in trajectory_list:
         for sim in simulations:
-            file.write(
-                f"-tt={trajectory['times']} --traj_x={trajectory['x']} --traj_y={trajectory['y']} "
-            )
-            file.write(
-                f"-tp={sim['output']}_{trajectory['name']}_{trajectory['times']} "
-            )
-            file.write(
-                f"-pd={ripdp_dir}{sim['output']}/rdp_{sim['output']} -od={traj_dir}{sim['output']}\n"
-            )
+            for diag in diags:
+                file.write(
+                    f"-tt={trajectory['times']} --traj_x={trajectory['x']} --traj_y={trajectory['y']} -td={diag} "
+                )
+                file.write(
+                    f"-tp={sim['output']}_{trajectory['name']}_{trajectory['times']} "
+                )
+                file.write(
+                    f"-pd={ripdp_dir}{sim['output']}/rdp_{sim['output']} -od={traj_dir}{sim['output']}\n"
+                )
         file.write("\n")
+
 
 # Create inputs for swarm Trajectories
 name = "SwarmTrajectories.inputs"
