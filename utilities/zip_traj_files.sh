605c59c80daae85aedca93e0dfec0cc13df32233
fherreazcue <francisco.herreazcue@gmail.com>
2024-06-13 14:15:35 +0100

    updates zip_traj_files utility to leave mp4 and zip files where they are.

--- utilities/zip_traj_files.sh
+++ utilities/zip_traj_files.sh
@@ -5,12 +5,15 @@ set -e
 # Parse the parent directory from args, or use current directory
 if [ $# -eq 0 ]; then
     parent_dir=$(pwd)
+    grandparent_dir=$(dirname $parent_dir)
+    cd $grandparent_dir
+    parent_dir=$(basename $parent_dir)
 else
     parent_dir=$1
 fi
 
-# Save the output of the find command in an array
-mapfile -t files < <(find $parent_dir -type f ! \( -name "*.pdf" -o -name "*.csv" \))
+# Create an array with all the file names in parent_dir, excluding pdf, csv, zip, and mp4 files
+mapfile -t files < <(find $parent_dir -type f ! \( -name "*.pdf" -o -name "*.csv" -o -name "*.zip" -o -name "*.mp4" \))
 
 # Create a zip file with the files in the array
 zip $parent_dir/$(basename $parent_dir)_trajectory_files.zip "${files[@]}"
