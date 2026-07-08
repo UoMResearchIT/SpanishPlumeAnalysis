fd5637ad375fe40286f5f1a596991dcda174ff25
fherreazcue <francisco.herreazcue@gmail.com>
2026-07-08 10:16:04 +0100

    reorganise wrf_widget to be a separate module and add metadata

---
+++ wrf_widget/README.md
@@ -0,0 +1,9 @@
+# WRF Widget
+
+This is a widget for visualizing WRF model output data.
+
+It can be installed as a separate module by running, from this directory, `pip install .`.
+
+It can then be imported as `import wrf_widget`.
+
+An example notebook is provided in the `examples` folder.
--- wrf_widget/wrf_widget/ipywidget.py
+++ wrf_widget/wrf_widget/ipywidget.py
@@ -3,12 +3,8 @@ import matplotlib.pyplot as plt
 import ipywidgets as widgets
 import numpy as np
 
-def view_wrf_3d(
-        da: xr.DataArray,
-        vmax=None,
-        vmin=None,
-        cmap="viridis"
-    ):
+
+def view_wrf_3d(da: xr.DataArray, vmax=None, vmin=None, cmap="viridis"):
     """
     Quick tool for viewing a 3D field from a WRF NetCDF file
     """
@@ -19,6 +15,7 @@ def view_wrf_3d(
     if vmax is None:
         vmax = np.nanmax(da)
     zdim = da.dims[0]
+
     @widgets.interact(level=(0, da.sizes[zdim] - 1))
     def view(level=0):
         fig, ax = plt.subplots(figsize=(9, 5))
@@ -28,19 +25,15 @@ def view_wrf_3d(
             cmap=cmap,
             aspect="auto",
             vmin=vmin,
-            vmax=vmax
+            vmax=vmax,
         )
         cbar = plt.colorbar(im, shrink=0.8, aspect=40)
         cbar.set_label(f"{da.name} [{da.units}]")
         plt.title(f"{da.description} Level {level}")
         plt.show()
 
-def view_wrf_2d(
-        da: xr.DataArray,
-        vmax=None,
-        vmin=None,
-        cmap="viridis"
-    ):
+
+def view_wrf_2d(da: xr.DataArray, vmax=None, vmin=None, cmap="viridis"):
     """
     Quick tool for viewing a 2D field from a WRF NetCDF file
     """
@@ -53,14 +46,7 @@ def view_wrf_2d(
         vmax = np.nanmax(da)
 
     fig, ax = plt.subplots(figsize=(9, 5))
-    im = ax.imshow(
-        da,
-        origin="lower",
-        cmap=cmap,
-        aspect="auto",
-        vmin=vmin,
-        vmax=vmax
-    )
+    im = ax.imshow(da, origin="lower", cmap=cmap, aspect="auto", vmin=vmin, vmax=vmax)
     cbar = plt.colorbar(im, shrink=0.8, aspect=40)
     cbar.set_label(f"{da.name} [{da.units}]")
     plt.title(f"{da.description}")
